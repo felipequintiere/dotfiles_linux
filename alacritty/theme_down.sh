eb825233136890b22755e2061d958af3f9391150
Epilef <fqnuvem@gmail.com>
2024-12-28 16:35:48 -0300

    test

---
+++ alacritty/theme_down.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $(cat $HOME/.config/alacritty/theme.log) == 132 ];then
+	xx=1
+	echo "1" > $HOME/.config/alacritty/theme.log
+else
+	x=$(cat $HOME/.config/alacritty/theme.log)
+	xx=$(($x+1))
+	echo "$(($x + 1))" > $HOME/.config/alacritty/theme.log
+fi
+
+y=$(sed -n "${xx}p" $HOME/.config/alacritty/theme)
+
+echo "[general]" > $HOME/.config/alacritty/alacritty.toml
+echo "import = [" >> $HOME/.config/alacritty/alacritty.toml
+echo "$y" >> $HOME/.config/alacritty/alacritty.toml
+echo "]" >> $HOME/.config/alacritty/alacritty.toml
+
+echo "[font]" >> $HOME/.config/alacritty/alacritty.toml
+echo "size = 16.00" >> $HOME/.config/alacritty/alacritty.toml
+
+opacity=$(cat $HOME/.config/alacritty/opacity.log)
+
+echo "[window]" >> $HOME/.config/alacritty/alacritty.toml
+echo "opacity = $opacity" >> $HOME/.config/alacritty/alacritty.toml
+
+
